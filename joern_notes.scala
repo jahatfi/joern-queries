d859fca57d0ceaea66b5054a5987fc40e11b25d9
James <jahafi@g.clemson.edu>
2022-04-24 01:06:23 -0400

    Good C sample for testing fuzzing data flow/taint analysis

---
+++ joern_notes.scala
@@ -0,0 +1,136 @@
+// References: 
+//https://jaiverma.github.io/blog/joern-uboot
+//https://jaiverma.github.io/blog/joern-intro
+//https://snoopysecurity.github.io/software-security/2021/12/08/joern-cheat-sheet.html
+
+
+cpg.call.name("system").astChildren.isExpression.astParent.l
+
+// Start here:
+run.ossdataflow
+
+// Combine query results: Get all local vars and parameters 
+(cpg.method.name("main").local ++ cpg.method.name("main").parameter).l
+
+//https://snoopysecurity.github.io/software-security/2021/12/08/joern-cheat-sheet.html
+// Also 
+def source = cpg.method.name(".*alloc.*").parameter  cpg.method.fullName("main").parameter
+
+//TODO Find tainted pointers
+// First, find arguments that are tainted,
+// TODO, find the variables they correspond to in the caller
+cpg.call.name("gets").argument.order(1).isArgument
+
+// Find the first path in layers.c
+// The line below creates a query to find all arguments passed TO gets
+val src = cpg.call.name("gets")
+// The line below creates a query to find all aruments passed TO system
+val sink = cpg.call.name("system")
+// Are any sinks reachable by sources?
+sink.reachableByFlows(src).p
+
+// Find the second path in layers.c
+// The line below creates a query to find all the first arguments passed TO source_3
+val src = cpg.call.name("source_3").argument.order(1).isArgument
+// The line below creates a query to find all aruments passed TO system
+val sink = cpg.call.name("system")
+// Are any sinks reachable by sources?
+sink.reachableByFlows(src).p
+
+// Rewrite the above more generically:
+
+// Enter the function below, then try this:
+// getPath2("source_3", "gets", "system") 
+// Only the first arg requires more analysis to find.
+// The other two are well known from the language
+def getPath2(   function_that_taints_param_ptr:String, 
+                source_that_taints_first_arg:String,
+                sink_first_arg:String) = {
+    //val function_that_taints_param_ptr = "source_3"
+    val src3_params = cpg.method.name(function_that_taints_param_ptr).parameter
+    val filteredParams = src3_params.filter(
+        param => {
+            //val src = method.parameter
+            val sink = cpg.method.ast.isCallTo(source_that_taints_first_arg).argument(1)
+            sink.reachableBy(param)
+        }.size > 0
+    )
+
+    val index_of_tainted_param = filteredParams.index.head
+
+    val src = cpg.call.name(function_that_taints_param_ptr).argument.order(index_of_tainted_param)
+
+    //val src = cpg.call.name("gets")
+    //val sink = cpg.call.name("system")
+    val sink = cpg.call.name(sink_first_arg)
+    sink.reachableByFlows(src).p
+}
+
+// I want to programatically find all functions that taint parameter pointers
+// TODO Pass in a dictionary of function names (e.g. {gets:0} to tainted indices
+// TODO Return a dictionary of function names to tainted indices, e.g. {source_3:0}
+def getFunctionsThatTaintParamPointers:String = {
+    val taintFirstIndex = "gets"
+    def candidateMethods = {cpg.method.name(taintFirstIndex).caller}
+    def filteredMethods = {candidateMethods.filter(
+        method => {
+            val sink = cpg.method.ast.isCallTo(taintFirstIndex).argument(1)
+            sink.reachableBy(method.parameter)        
+        }.size > 0
+    ).name.head}
+    return filteredMethods
+}
+getPath2(getFunctionsThatTaintParamPointers, "gets", "system") 
+
+
+// This scala method will find functions that 
+// receive an argument affected by gets() and 
+// pass that argument to system()
+// Therefore main() is excluded, as Joern is examining
+// the parameters passed in (e.g. argv, argc)
+def getFlow() = {
+    val methods = cpg.method.name("system").caller
+    val filteredMethods = methods.filter(
+        method => {
+            val src = method.parameter
+            val sink = method.ast.isCallTo("system").argument(1)
+            sink.reachableBy(src)
+        }.size > 0
+    )
+
+    val src = cpg.call.name("gets")
+    val sink = filteredMethods.parameter.argument
+    sink.reachableByFlows(src).p
+}
+
+// This scala method will find functions that 
+// receive an argument affected by gets() and 
+// pass that argument to system()
+// Therefore main() is excluded, as Joern is examining
+// the parameters passed in (e.g. argv, argc)
+def getFlow2() = {
+    val sink_parent_methods = cpg.method.name("system").caller
+    val filteredSinkPMethods = sink_parent_methods.filter(
+        method => {
+            val tainted_buff_ptr = cpg.call.name("gets").argument.order(1)
+            val sink = method.ast.isCallTo("system").argument(1)
+            sink.reachableBy(tainted_buff_ptr)
+        }.size > 0
+    )
+
+    val src_parent_methods = cpg.method.name("gets").caller
+    val filteredSrcPMethods = src_parent_methods.filter(
+        method => {
+            val tainted_buff_ptr = method.call.name("gets").argument.order(1).isParameter
+            val sink = cpg.call.name("system").argument(1)
+            sink.reachableBy(tainted_buff_ptr)
+        }.size > 0
+    )    
+
+    //val src = cpg.call.name("gets")
+    //val src = cpg.method.name("gets").caller
+    //val sink = filteredMethods.parameter ++ filteredMethods.local
+    //val sink = filteredMethods.local
+
+    filteredSinkPMethods.reachableByFlows(filteredSrcPMethods).p
+}
---
+++ layers.c
@@ -0,0 +1,432 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include "defs.h"
+//#include "test.h"
+
+//Verfiy preprocessing is performed
+#define PI 3.14
+int global_int = 6;
+
+//Function prototypes (normally in a .h file)
+char source_1();
+int source_2(int z);
+void source_3(char *buff, int len);
+int layer_1_a(int a, char *buff);
+int layer_1_b(bool flag, char *buff, float c, double d);
+void layer_1_c(char * buff, int b);
+int layer_2_a(char *buff, bool flag);
+bool layer_2_b(char * buff, int myInt);
+char * layer_2_c(char * buff, bool flag);
+bool layer_3_a(char * buff, int count, bool flag);
+int layer_3_b(bool flag, char *buff);
+void sink_1(char * buff);
+void sink_2(char * buff);
+void sink_3();
+//void sink_4();
+
+char * return_tainted_buff(){
+    //Allocate a buffer on the heap.  Taint it, the return it.
+    char * tainted_buff_ptr = NULL;
+    int size = 1024 * 2;
+    tainted_buff_ptr = (char *) malloc(size*sizeof(char));
+    if (!tainted_buff_ptr) return NULL;
+    gets(tainted_buff_ptr);
+    return tainted_buff_ptr;
+}
+
+void readme(int a){
+    int b = a;
+    system(b);
+}
+
+//Begin source functions
+char
+source_1 (){
+    /* A test */ /*Another test */ /*Another test */     /* A test */ /*Another test */ /*Another test */
+
+   char *data;
+   char buff[100];
+   char buff2[100];
+   char buff3[100];
+   int a, b;  //Tests that I can parse variables declared on the same line
+   float c;
+   int d = rand();
+   int e = rand();
+   float pi = PI;  //Test macro expansion
+   float tau = TAU;//Test inclusion of macro def from external file
+   bool flag;
+   double myDouble;
+   char *myBuff;
+   //Will return buff[0].  Want to be sure that it's initialized.
+   buff[0] = 'A';
+   goto test_label;
+   return 'E';  //Should never be reached
+   test_label:
+   {
+       int test;
+       //This is a plain block idiom.  You SHOULD see this in the resulting file, but it should NOT be indented.
+   }
+   printf("Enter some data (< 100 chars):\n");
+   gets(buff);
+   //fgets(buff, d, a);
+   goto test_label_1;
+   printf("This should not be printed.");
+   test_label_1:
+   if(1 == 1){
+       printf("I must be here!\n"); //This line must not be removed from a path
+   }
+   if(d < 0){
+      layer_1_a (a,buff);           //Test whitespace between predicate and '('
+   }
+   if(e > 0) layer_1_b(flag, buff, c, myDouble);
+   layer_1_c(buff, e);
+   return buff[0];
+}
+//Testing conditional compilation
+#define CONCOM
+#ifdef CONCOM
+    int
+    source_2(int z)
+    {
+    int a = z;
+    int b;
+    int i = 0;
+    int numBytes;
+    int socketfd = 1;
+    int buffSize = 256;
+    char *myBuff;
+    char myBuff2[512];
+    bool flag = false;
+    float f = 4.5;
+    double d = 1.2;
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    for(i = 0; i++; i < 10){
+            //numBytes = recv(socketfd, myBuff2, buffSize, 0);
+            numBytes = read(socketfd, myBuff2, buffSize);            
+            myBuff = (char *)&myBuff2;
+            //Call layer1 function
+            layer_1_b(flag, myBuff, f, d);
+    }
+    return numBytes;
+    }
+#endif
+
+//===========================================================
+void source_3(char *source_3_buff, int len){
+    int a;
+    int count;
+    int numBytes;
+    int file_fd = 1;
+
+    //printf("Enter some data fewer than %d chars\n",len);
+    //numBytes = read(stdin, buff, len);
+    //Call layer1 function
+    gets(source_3_buff);
+}
+//==============================================================================
+//This is a dead end
+int layer_1_a(int a, char *buff){
+   int x = rand();
+   int i = 0;
+   float y = 3.14;
+   if(buff[0] == 'A'){
+       printf("The first char is 'A'\n");
+       printf("x: %d\n", x);
+   }
+   else{
+
+       printf("The first char is not 'A'\n");
+       printf("y: %f\n", y);
+   }
+   printf("Hmm"); for(i = 0; i < 5; i++){
+      printf("Hi\n");
+   }
+   return 0;
+}
+//==============================================================================
+//Not a dead end
+int
+layer_1_b(bool flag,
+          char *buff,
+          float c,
+          double d)
+          {
+   double myDouble;
+   char *newBuff;
+   //Call layer2
+   if(c > 75){
+       if(d <= 4){
+           printf("d Must be greater than 4.\n");
+           if(d <= 6){
+               printf("A totally unecessary level of nested code.\n  This should get removed.");
+           }
+           return -1;
+       }
+       else{
+           printf("Success: Yes.\n");
+           layer_2_b(buff, flag);
+           return 0;
+       }
+   }
+
+   else{
+       printf("C must exceed 75.\n");
+       return -2;
+   }
+
+}
+//==============================================================================
+//This is not a dead end
+void layer_1_c(char * buff, int b){
+    char myChar;
+    bool flag;
+    if(b <  0) return;
+    if(b >= 0 && b <= 50){
+        //Call layer2
+        flag = layer_2_b(buff, b);
+    }
+    if(b > 50){
+       if(b < 250 ){
+           //Call layer2
+           flag = layer_2_c(buff, flag);
+       }
+       else{
+           printf("b must be less than 250.\n");
+       }
+    }
+    else return;
+}
+
+//Begin layer 2 functions
+
+//A dead end
+int layer_2_a(char *buff, bool flag){
+    if(!flag){
+        return 1;
+    }
+    if((char)buff[1] == 'B'){
+        printf("2nd char of Buff is 'B'");
+    }
+    return 0;
+}
+//==============================================================================
+//Not a dead end
+bool layer_2_b(char * buff, int myInt){
+    int count = rand();
+    int randNum1 = rand();
+    bool flag = false;
+
+    if(randNum1 < 5) flag = true;
+
+    if(flag){
+       if(count <= 5){
+          printf("Count must be greater than 5.\n");
+       }
+       else{
+           layer_3_a(buff, count, flag);
+       }
+    }
+    else printf("Flag must be true.\n");
+
+    return flag;
+}
+
+//Also not a dead end
+char * layer_2_c(char * buff, bool flag){
+   if(flag){
+       layer_3_b(flag, buff);
+   }
+}
+//==============================================================================
+//Begin layer 3 functions
+bool layer_3_a(char * buff, int count, bool flag){
+
+   if(count > 128){
+      if(flag){
+          printf("Flag must be false.\n");
+          return false;
+      }
+      else if(count > 64){
+          sink_1(buff);
+          return true;
+      }
+   }
+}
+//==============================================================================
+int layer_3_b(bool flag, char *buff){
+   srand(100);
+   int myVar = rand();
+   if(myVar <= 10000){
+      if(myVar <= 0){
+          printf("myVar must be greater than 0\n");
+          return -1;
+      }
+      else{
+          sink_1(buff);
+          return 0;
+      }
+   }
+   //Dead code here
+   switch(myVar){
+       case 0:
+          printf("hi");
+       case 1:
+          sink_2(buff);
+          break;
+       case 2:
+          sink_1(buff);
+          break;
+       default:
+          break;
+   }
+}
+//==============================================================================
+
+//Simple sink function
+void sink_1(char * buff){
+    system(buff);
+    return;
+}
+//==============================================================================
+
+//Simple sink function
+void sink_2(char * buff){
+    char dest[100];
+    sprintf(dest, "%s", buff);
+    //system(buff);
+    return;
+}
+//==============================================================================
+
+void sink_3(){
+    //Unreachable.  Should be excluded.
+    char buff[100];
+    gets(buff);
+    system(buff);
+}
+//==============================================================================
+void sink_4(char* buff, int len){
+    char dest[100];
+    memcpy(dest, buff, len);
+}
+//==============================================================================
+
+void main (int argc, char * argv[]){
+    //TODO Taint a struct, and some dynamically allocated memory
+
+    // Path #1 is simply the next 3 lines
+    char * buff1 = return_tainted_buff();
+    system(buff1);
+    free(buff1);
+
+    // Path #2 is simply the next 4 lines
+    int len = 256;
+    char buff[len];
+    source_3(buff, len);
+    system(buff);
+
+    printf("The global int is %d\n", global_int);
+    int i;
+    i++;
+    i--;
+    ++i;
+    i--;
+    int my_int, my_int2;    
+
+
+
+    strcpy(buff, argv[1]); 
+    for(i = 0; i < len; i++){
+        buff[i] = (char) my_int;
+
+    }    
+    int (*source_2_ptr)(int) = &source_2;
+  
+    if(1){
+        if(0){
+            printf("Dead code\n");
+        }
+    }
+
+    printf("Testing...\n");
+    source_1();
+    //source_2(1);
+    (*source_2)(1);  //SIFT appears to work with function pointers
+    source_3(buff, len);
+
+    strcpy(buff, argv[1]);  //This is bad code.
+    fscanf(stdin, "%d%d", &my_int, &my_int2); 
+    readme(my_int);
+
+    my_int2 = len; //my_int2 is sanitized here via constant overwrite
+    sink_4(buff, my_int);
+
+
+    if(my_int) len = my_int;
+    if(buff[0]) len = my_int2;
+    printf("Done.\n");
+    return;
+}
+//==============================================================================
+
+
+
+
+/* Need to account for various styles
+    The following are different styles to take into account:
+    1. Placement of return type         (same line T1__ or line above  T2__ )
+    2. Placement of parameters          (same line T_1_ or one each    T_2_ )
+    3. Placement of opening bracket '{' (same line T__1 or line below  T__2)
+
+    T1__:
+        T111: int foo(...){
+        T112: int foo(...)
+              {
+        T121: int foo(.
+                .
+                .){
+        T122: int foo(.
+                .
+                .)
+                {
+    T2__:
+        T211:   int
+                foo(...){
+        T212:   int
+                foo(...)
+                {
+        T221:   int
+                foo(.
+                .
+                .){
+        T222:   int
+                foo(.
+                .
+                .)
+                {
+    Also note that all code in a .c file may not be function definitions.
+    There may be structions, enums, etc.
+    e.g.
+
+    void foo(){}
+
+    enum modes { MODE_INVALID, MODE_CHUNKED, MODE_ADDSTUFF, MODE_NORMAL };
+
+    const struct {
+        const enum modes mode;
+        const char * text;
+    } modes_array[] = {
+        {MODE_CHUNKED, "chunked"},
+        {MODE_ADDSTUFF, "addstuff"},
+        {MODE_NORMAL, "normal"},
+        {MODE_INVALID, NULL}
+    };
+
+    int bar(){}
+*/
+
+
